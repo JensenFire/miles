263bb3300e09851784f6c33186e973cd26fa6d6d
xinji1 <xinji1@microsoft.com>
2026-04-02 18:12:59 +0800

    revert

--- examples/p2p_weight_transfer/run-kimi-k2-instruct-64node-profile.sh
+++ examples/p2p_weight_transfer/run-kimi-k2-instruct-64node-profile.sh
@@ -252,8 +252,7 @@ run_mode() {
     \"PYTHONPATH\": \"/root/Megatron-LM/\",
     \"CUDA_DEVICE_MAX_CONNECTIONS\": \"1\",
     \"NCCL_NVLS_ENABLE\": \"${NCCL_NVLS_VAL}\",
-    \"MODEL_ARGS_ROTARY_BASE\": \"50000\",
-    \"DEPRECATED_MEGATRON_COMPATIBLE\": \"1\"
+    \"MODEL_ARGS_ROTARY_BASE\": \"50000\"
   }
 }"
 
