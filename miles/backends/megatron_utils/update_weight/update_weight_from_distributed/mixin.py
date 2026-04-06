dc6ca9e47a3fe75ca8e6f022a8922c8eb786abc1
JensenFire <elevenji@outlook.com>
2026-04-06 20:59:36 +0800

    temp

--- miles/backends/megatron_utils/update_weight/update_weight_from_distributed/mixin.py
+++ miles/backends/megatron_utils/update_weight/update_weight_from_distributed/mixin.py
@@ -154,15 +154,15 @@ class DistBucketedWeightUpdateMixin:
         """Run post-process if needed and resume rollout engines."""
         if dist.get_rank() == 0:
             # int4/fp4 post_process, mxfp8 post-process (swizzle MoE scales).
-            if self.quantization_config and self.quantization_config["quant_method"] in [
-                "compressed-tensors",
-                "mxfp8",
-            ]:
-                post_process_weights(
-                    rollout_engines=self.rollout_engines,
-                    restore_weights_before_load=False,
-                    post_process_quantization=True,
-                )
+            # if self.quantization_config and self.quantization_config["quant_method"] in [
+            #     "compressed-tensors",
+            #     "mxfp8",
+            # ]:
+            post_process_weights(
+                rollout_engines=self.rollout_engines,
+                restore_weights_before_load=False,
+                post_process_quantization=True,
+            )
             ray.get([engine.continue_generation.remote() for engine in self.rollout_engines])
 
     @torch.no_grad()
